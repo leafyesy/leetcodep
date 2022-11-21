24fd29a2bed65aace6f12a3ed86842d783c2968c
shouyouye <shouyouye@tencent.com>
2022-11-21 23:31:51 +0800

    leetcode-53 -动态规划

--- src/leet2022/LeetCode53.java
+++ src/leet2022/LeetCode53.java
@@ -36,7 +36,23 @@ public class LeetCode53 {
     }
 
 
+    //f(x) = Math.max(f(x - 1) + nums[x], nums[x])
     public int maxSubArray2(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int result = dp[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            if (dp[i] > result) result = dp[i];
+        }
+        return result;
+    }
+
+
+    public int maxSubArray3(int[] nums) {
         if (nums.length == 0) {
             return 0;
         }
